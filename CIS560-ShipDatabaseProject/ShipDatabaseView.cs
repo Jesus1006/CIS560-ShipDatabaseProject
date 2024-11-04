37fd47c4475c3f5ab3d035f17c2f37fd0cee1368
Jesus Castro-Garcia <castro.jesus1006@gmail.com>
2024-11-04 14:22:01 -0600

    commit changes

---
+++ CIS560-ShipDatabaseProject/ShipDatabaseView.cs
@@ -0,0 +1,10 @@
+namespace CIS560_ShipDatabaseProject
+{
+    public partial class ShipDatabaseView : Form
+    {
+        public ShipDatabaseView()
+        {
+            InitializeComponent();
+        }
+    }
+}
